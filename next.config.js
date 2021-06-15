8db5eb7a0639d5a09e504b869c99f8d156dd8e7a
jiyuujin <jiyuujinunite@gmail.com>
2021-06-16 08:37:09 +0900

    Next.js 11 - resolved #1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const nextConfig = {
     target: 'serverless',
+    eslint: {
+        // TODO: false to be enable eslint-config-next
+        ignoreDuringBuilds: true,
+    },
     env: {
         NEXT_ROUTINE_API: process.env.NEXT_ROUTINE_API,
     },
